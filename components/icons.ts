8e580d8b86d95ec0aa4765e316c0b6f4cb4d837d
Gorisi Deepak Reddy <thedeepakreddy1@gmail.com>
2026-06-07 00:36:15 +0200

    Delete components directory

--- components/icons.ts
+++
@@ -1,8 +0,0 @@
-import { Sparkles, Scissors, Wrench } from 'lucide-react';
-import React from 'react';
-
-export const IconMap: Record<string, React.FC<any>> = {
-  Sparkles,
-  Scissors,
-  Wrench
-};
